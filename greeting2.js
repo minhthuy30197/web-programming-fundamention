5c243e3dc74e71b575ebb5e3c18c0d1548f3180e
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-23 17:25:22 +0700

    return time when access url '/chat?Username=BinhSon&ask=time'

--- greeting2.js
+++ greeting2.js
@@ -16,6 +16,14 @@ let webApp = http.createServer(function (req, res) {
           let second = date.getSeconds()
           res.end(`Hello, BinhSon. It's ${time}:${minute}:${second} now`)
         }
+        else
+          if (url === '/chat?ask=time') {
+            let date = new Date();
+            let time = date.getHours()
+            let minute = date.getMinutes()
+            let second = date.getSeconds()
+            res.end(`Hello. It's ${time}:${minute}:${second} now`)
+          }
   res.writeHeader(200, {'Content-Type':'text/plain'})
 })
 
