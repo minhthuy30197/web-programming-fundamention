78019721ee3133c760d65cbdecc301898d5b6280
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-24 18:25:03 +0700

    routing express into '/chat'

--- lesson-02-expres-routing/greeting3.js
+++ lesson-02-expres-routing/greeting3.js
@@ -7,6 +7,10 @@ app.get('/', (req,res) => {
   res.status(200).send('greeting')
 })
 
+app.get('/chat', (req,res) => {
+  res.status(200).send('Hello')
+})
+
 app.listen(8000, function () {
   console.log('Listening at port 8000')
 })
