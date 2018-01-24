0652e428cd4657150d419b05f272b65fe1825b91
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-24 18:23:53 +0700

    routing express into '/'

--- lesson-02-expres-routing/greeting3.js
+++ lesson-02-expres-routing/greeting3.js
@@ -3,6 +3,10 @@ let express = require('express')
 
 let app = new express()
 
+app.get('/', (req,res) => {
+  res.status(200).send('greeting')
+})
+
 app.listen(8000, function () {
   console.log('Listening at port 8000')
 })
