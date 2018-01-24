66ac3042af19bb1479f68402ca3bae577f49b765
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-24 20:56:06 +0700

    routing express into '/chat?username=BinhSon&ask=time'

--- lesson-02-expres-routing/greeting3.js
+++ lesson-02-expres-routing/greeting3.js
@@ -9,8 +9,17 @@ app.get('/', (req,res) => {
 
 app.get('/chat', (req,res) => {
   let username = req.param('username')
-  if (username === undefined) res.status(200).send('Hello')
-  else res.status(200).send('Hello, BinhSon')
+  let ask = req.param('ask')
+  if ((username === undefined) && (ask === undefined)) res.status(200).send('Hello')
+  else
+    if (ask === undefined) res.status(200).send('Hello, BinhSon')
+    else {
+      let date = new Date();
+      let time = date.getHours()
+      let minute = date.getMinutes()
+      let second = date.getSeconds()
+      res.status(200).send(`Hello, BinhSon. It's ${time}:${minute}:${second} now`)
+    }
 })
 
 app.listen(8000, function () {
