325e416e990aa47c60c5d36b6777e03802aa727a
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-24 18:18:00 +0700

    routing with express

--- lesson-02-expres-routing/app.js
+++ lesson-02-expres-routing/app.js
@@ -2,6 +2,14 @@ let express = require('express')
 
 let app = new express()
 
+app.get('/',function (req,res) {
+  res.status(200).send('hello')
+})
+
+app.get('/about',function (req,res) {
+  res.status(200).send('Hello! My name is Thuy')
+})
+
 app.listen(8000,function () {
   console.log('Listening at port 8000')
 })
