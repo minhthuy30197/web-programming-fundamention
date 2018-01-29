03277877e2c641e7143a53d8f2f0bf1f095fc953
minhthuy30197 <minhthuy30197@gmail.com>
2018-01-29 17:39:58 +0700

    demo template engine using pug

---
+++ lesson-03-template-engine/demo-pug.js
@@ -0,0 +1,15 @@
+'use strict'
+let express = require('express')
+
+let app = new express()
+app.set('view engine','pug')
+
+app.get('/', function (req, res) {
+  let view = 'greeting'
+  let param = {name: 'Thuy'}
+  res.render(view, param)
+})
+
+app.listen(8000, function () {
+  console.log('Listening at port 8000')
+})
